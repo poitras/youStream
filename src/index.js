d7d458c9c2b3d76c398d11eab0e2df849402110d
poitras <nicopoitras@gmail.com>
2017-12-12 22:55:36 -0500

    Youtube api key added + search_bar components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+                      // ./ mean in the current directory go components ...
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCAFOyZ3z4eBAg35LfOuDWGM2SvBSdBvyM'
+
 // import { Provider } from 'react-redux';
 // import { createStore, applyMiddleware } from 'redux';
 
@@ -16,7 +21,11 @@ import ReactDOM from 'react-dom';
 
 // Create new component that will produce html
 const App = () => {
-  return <div>Hi!</div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  ) 
 }
 
 // React take this component and put it in the DOM
